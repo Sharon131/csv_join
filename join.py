6af6c753890c941f3783d61c4709f8824b1d0c09
Magdalena Pastuła <pastula.magdalena@gmail.com>
2022-04-14 21:39:56 +0200

    Added left and right join.

--- join.py
+++ join.py
@@ -16,12 +16,42 @@ def print_joined(row1, row2, columns1, columns2, column_name):
         print(row1[col1], end='\t')
     for col2 in columns2:
         if col2 != column_name:
-            print(row2[col2], end='\t')
+            if row2.empty:
+                print("NULL", end='\t')
+            else:
+                print(row2[col2], end='\t')
     print("")
 
 
-def left_join(filename1, filename2):
-    print("Left join")
+def left_join(filename1, filename2, column_name, nrows=50):
+    print("Left join:")
+    df1 = pandas.read_csv(filename1, nrows=nrows)
+    df2 = pandas.read_csv(filename2, nrows=nrows)
+    columns1 = df1.columns.to_list()
+    columns2 = df2.columns.to_list()
+    iter1 = 0
+    iter2 = 0
+
+    print_header(df1.columns.to_list(), df2.columns.to_list(), column_name)
+    while not df1.empty:
+        flags = [False for i in range(nrows)]
+        while not df2.empty:
+            for i, row1 in df1.iterrows():
+                for j, row2 in df2.iterrows():
+                    if row1[column_name] == row2[column_name]:
+                        flags[i] = True
+                        print_joined(row1, row2, columns1, columns2, column_name)
+            iter2 += 1
+            df2 = pandas.read_csv(filename2, nrows=nrows, skiprows=nrows * iter2 + 1, names=columns2)
+        for i in range(len(flags)):
+            if not flags[i]:
+                print_joined(df1.iloc[i], pandas.Series([], dtype=object), columns1, columns2, column_name)
+        iter2 = 0
+        iter1 += 1
+        df1 = pandas.read_csv(filename1, nrows=nrows, skiprows=nrows * iter1 + 1, names=columns1)
+        df2 = pandas.read_csv(filename2, nrows=nrows)
+
+    print("-----")
 
 
 def inner_join(filename1, filename2, column_name, nrows=50):
@@ -50,8 +80,8 @@ def inner_join(filename1, filename2, column_name, nrows=50):
     print("-----")
 
 
-def right_join(file1, file2):
-    pass
+def right_join(filename1, filename2, column_name, nrows=50):
+    left_join(filename2, filename1, column_name, nrows)
 
 
 joins = {
